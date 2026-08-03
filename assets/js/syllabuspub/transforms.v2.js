35485d473e4b2db466760384a961e0c5685b4fc7
Greg Johnson <54258105+loighic@users.noreply.github.com>
2026-08-03 18:11:46 -0500

    Update transforms.v2.js

--- assets/js/syllabuspub/transforms.v2.js
+++ assets/js/syllabuspub/transforms.v2.js
@@ -67,6 +67,7 @@
       this.affiliation = object.affiliation; // 'Google Brain'
       this.affiliationURL = object.affiliationURL; // 'https://g.co/brain'
       this.affiliations = object.affiliations || []; // new-style affiliations
+      this.email = object.email; // 'name@example.edu'
     }
 
     // 'Chris'
